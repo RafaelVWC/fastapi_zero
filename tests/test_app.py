a01dcdd6194a8f730a476f3d4afce8273400d355
Rafael <rafaelvianna2001@gmail.com>
2026-02-11 10:13:57 -0300

    AULA 01 : Criando repositório

---
+++ tests/test_app.py
@@ -0,0 +1,16 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fastapi_zero.app import app
+
+
+
+
+def test_root_deve_retornar_hello_world():
+    client = TestClient(app)
+
+    response = client.get('/')
+    
+    assert response.json() == {'message': 'Hello World!'}
+    assert response.status_code == HTTPStatus.OK      
